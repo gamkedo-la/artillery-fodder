8af58bab8798cb9289ff73a84fe327b79795af31
sjhoffiz <sj.hoffiz@gmail.com>
2020-01-29 14:07:54 -0500

    Rearranged menu order.

--- js/Scenes/MainMenu.js
+++ js/Scenes/MainMenu.js
@@ -3,13 +3,20 @@
 function modeMainMenu(frameTime) {
 	colorRect(0, 0, canvas.width, canvas.height, "LightGrey");	
 	colorText("MAIN MENU", canvas.width/2 - 160, 100, "White", "50px Arial");
+
 	colorText("Press [Space Bar] to START GAME", canvas.width/2 - 150, canvas.height/2, "Black", "20px Arial");
-	colorText("Press [C] for CREDITS", canvas.width/2 - 150, canvas.height/2 + 30, "Black", "20px Arial");
-	colorText("Press [L] for CONTROLS", canvas.width/2 - 150, canvas.height/2 + 60, "Black", "20px Arial");
+
+	colorText("Press [P] for PLAYER SCREEN", canvas.width/2 - 150, canvas.height/2 + 30, "Black", "20px Arial");
+
+	colorText("Press [I] for WEAPON INVENTORY", canvas.width/2 - 150, canvas.height/2 + 60, "Black", "20px Arial");
+	
 	colorText("Press [T] for TERRAIN SCREEN", canvas.width/2 - 150, canvas.height/2 + 90, "Black", "20px Arial");
-	colorText("Press [P] for PLAYER SCREEN", canvas.width/2 - 150, canvas.height/2 + 120, "Black", "20px Arial");
+
+	colorText("Press [L] for CONTROLS", canvas.width/2 - 150, canvas.height/2 + 120, "Black", "20px Arial");
+
 	colorText("Press [O] for OPTIONS", canvas.width/2 - 150, canvas.height/2 + 150, "Black", "20px Arial");
-	colorText("Press [I] for WEAPON INVENTORY", canvas.width/2 - 150, canvas.height/2 + 180, "Black", "20px Arial");
+
+	colorText("Press [C] for CREDITS", canvas.width/2 - 150, canvas.height/2 + 180, "Black", "20px Arial");
 
 	if (Key.isJustPressed(Key.SPACE)){
 		mode = GAME_MODE;
