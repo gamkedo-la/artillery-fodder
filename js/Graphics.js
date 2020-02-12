d3d8e5b8748f2c9f246bb81ef62cfe7c6fffce20
Michael Fewkes <mdfewkes@gmail.com>
2020-02-12 14:12:53 -0700

    Consolidated graphics code

--- js/Graphics.js
+++ js/Graphics.js
@@ -1,3 +1,39 @@
+const imageLoader = new (function() {
+	const IMAGE_DEFS = [
+		{id: "cloud1", src: "./images/cloud1.png"},
+		{id: "tankSpriteSheet", src: "./images/tankSkins.png"},
+	];
+	const images = {};
+
+	this.loadImages = function() {
+		return Promise.all(IMAGE_DEFS.map(function(imageDef) {
+			return new Promise(function(resolve, reject) {
+				const image = new Image();
+				image.onload = function() {
+					resolve({id: imageDef.id, image: image});
+				}
+				image.onerror = function() {
+					reject(image);
+				}
+				image.src = imageDef.src;
+			});
+		})).then(function(values) {
+			values.forEach(function(value) {
+				images[value.id] = value.image;
+			});
+			Object.freeze(images);
+		});
+	};
+
+	this.getImage = function(id) {
+		return images[id];
+	};
+
+	this.getAllImages = function() {
+		return images;
+	};
+})();
+
 function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
 	canvasContext.fillStyle = fillColor;
 	canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
