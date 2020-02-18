38a11e5778b3fc591a903aca7be29603fcb31ada
sjhoffiz <sj.hoffiz@gmail.com>
2020-02-18 13:32:32 -0500

    Cleaning up code and aesthetic modifications to title screen

--- js/Scenes/TitleScreen.js
+++ js/Scenes/TitleScreen.js
@@ -5,11 +5,8 @@ function modeTitle(frameTime) {
 
 	titleScreenImg = imageLoader.getImage("titleScreenCover");
 
-	colorRect(0, 0, canvas.width, canvas.height, "LightGrey");	
-	colorRect(100, 100, canvas.width-200, canvas.height-200, "Grey");
-	colorText("Artillery Fodder", canvas.width/2, canvas.height/2, "White", "50px Arial");
 	canvasContext.drawImage(titleScreenImg, 0, 0);
-	colorText("Press [Space Bar] to START", canvas.width/2, canvas.height/2 + 100, "Black", "20px Arial");
+	colorText("[Space Bar] to START", canvas.width/2, 530, "Black", "30px Arial");
 
     if (Key.isJustPressed(Key.SPACE)
         || SpeechRecognition.pendingStartCommand()
