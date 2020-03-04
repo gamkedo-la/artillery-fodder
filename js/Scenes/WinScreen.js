b274c57949270c21ebe5543558a4e3ecd1a484be
Christer Kaitila <breakdance@mcfunkypants.com>
2020-03-04 11:32:45 -0800

    winscreen stats animation implemented

--- js/Scenes/WinScreen.js
+++ js/Scenes/WinScreen.js
@@ -3,13 +3,15 @@
 function modeWinScreen(frameTime) {
 	canvasContext.drawImage (imageLoader.getImage("winScreenBackground"), 0, 0);
 	//colorRect(0, 0, canvas.width, canvas.height, "blue");
-	colorText("WIN SCREEN", canvas.width/2, 100, "White", "50px Arial");
-	colorText("[Space Bar] MAIN MENU", canvas.width/2, canvas.height - 50, "white", "20px Arial");
+	colorText("WIN SCREEN", canvas.width/2, 100, "White", "50px Arial",true);
+    colorText("[Space Bar] MAIN MENU", canvas.width/2, canvas.height - 50, "white", "20px Arial", true);
+    
+    stats.drawWinScreen();
 
 	if (Key.isJustPressed(Key.SPACE) 
 		|| Key.isJustPressed(Key.q)
 		|| (isMouseInArea(0, canvas.height - 50, canvas.width, 50) && mouseJustPressed)){
-		mode = MAIN_MENU;
+        mode = MAIN_MENU;
 		map.init(canvas.width, canvas.height-UI_HEIGHT);
 		startMatch();
 	}
--- js/Stats.js
+++ js/Stats.js
@@ -10,6 +10,33 @@ var stats = {
     hits:0,
     frames:0,
     explosions:0,
+    winScreensInARow:0,
+
+    drawWinScreen: function() {
+        
+        var x = 600; // starting pos
+        var y = 200;
+        var h = 16; // line height
+        var c = "white"; // colour
+        var f = "16px Courier"; // font
+        var s = true; // shadow
+        var n = 1; // line number
+        var d = 30; // delay frames per line
+
+        this.winScreensInARow++;
+
+        // vertical, animated "count-up"
+        if (this.winScreensInARow>d*n) colorText("Endgame Stats:",x,y,c,f,s);
+        y += h; // blank line
+        if (this.winScreensInARow>d*n) colorText(this.turns.toFixed(0)+" turns",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.shots.toFixed(0)+" shots",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.hits.toFixed(0)+" hits",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.misses.toFixed(0)+" misses",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.explosions.toFixed(0)+" explosions",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.damage.toFixed(0)+" damage",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.seconds.toFixed(1)+" seconds",x,y+(h*n++),c,f,s);
+        if (this.winScreensInARow>d*n) colorText(this.frames.toFixed(0)+" frames",x,y+(h*n++),c,f,s);
+    },
 
     draw: function(largemode=false) {
 
@@ -20,19 +47,9 @@ var stats = {
         var f = "9px Courier"; // font
         var s = true; // shadow
         var n = 1; // line number
+        
+        this.winScreensInARow = 0; // possibly reset from prev game
 
-        // vertical
-        if (largemode) {
-            colorText("Endgame Stats:",x,y,c,f,s);
-            colorText(this.turns.toFixed(0)+" turns",x,y+(h*n++),c,f,s);
-            colorText(this.shots.toFixed(0)+" shots",x,y+(h*n++),c,f,s);
-            colorText(this.hits.toFixed(0)+" hits",x,y+(h*n++),c,f,s);
-            colorText(this.misses.toFixed(0)+" misses",x,y+(h*n++),c,f,s);
-            colorText(this.explosions.toFixed(0)+" explosions",x,y+(h*n++),c,f,s);
-            colorText(this.damage.toFixed(0)+" damage",x,y+(h*n++),c,f,s);
-            colorText(this.seconds.toFixed(1)+" seconds",x,y+(h*n++),c,f,s);
-            colorText(this.frames.toFixed(0)+" frames",x,y+(h*n++),c,f,s);
-        } else { 
         // mini stats display
         colorText(
             //"STATS: "
@@ -47,7 +64,6 @@ var stats = {
             //+ this.seconds.toFixed(1)+" seconds "
             //+ this.frames.toFixed(0)+" frames "
             ,x,y,c,f,s);
-        }
     }
 };
 
