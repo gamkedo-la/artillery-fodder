4db5cc07c5555fb211bf9d861bc9aea5ae2bf9f3
EyeForcz <61577998+EyeForcz@users.noreply.github.com>
2020-03-06 23:37:34 +0100

    credits screen fixed and tweaked

--- js/Scenes/CreditScreen.js
+++ js/Scenes/CreditScreen.js
@@ -1,20 +1,23 @@
 //Credit Screen
 
-var posHeight = 100;
-var count = 0;
+
 var creditNameList = ['Toe Nail', 'Tommy Vercetti', 'Avril Lavigne', 'Input Name here', 'Jimmy Strong', 'Raymond Reddington', 
-					'Anya Olsen', 'Recep Ivedik', 'HomeTeam', 'Bitcooooooonect', 'Chris DeLeooooooooooooon', 'Cmon a few names more',
+					'Anya Olsen', 'Recep Ivedik', 'HomeTeam', 'Bitcooooooonect', 'Chris DeLeooooooooooon', 'Cmon a few names more',
 					'here is one', 'a few left', 'Will it Blender', 'Lucid dreams'];
 
 function modeCredits(frameTime) {
+	var posHeight = 180;
+	var count = 0;
 	canvasContext.drawImage (imageLoader.getImage("creditScreenBackground"), 0, 0);
 
-	while (count < creditNameList.length){
-		colorText(creditNameList[count], canvas.width/2, posHeight, "yellow", "20px Arial");
-		posHeight = posHeight+20;
-		count++;
-	};
-
+	for (count; count < creditNameList.length/2; count++){
+		colorText(creditNameList[count], 230, posHeight+count*35, "white", "27px Arial");
+	}
+	
+	for (count; count < creditNameList.length; count++){
+		colorText(creditNameList[count], 570, posHeight+(count-creditNameList.length/2)*35, "white", "27px Arial");
+	}
+	
 	btnManager.mainMenuButton.draw()
 
 }
