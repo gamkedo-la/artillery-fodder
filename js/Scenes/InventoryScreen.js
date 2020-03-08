2855bea6f99205c8c7ebd1bac015bc3cb9d00972
Michael Fewkes <mdfewkes@gmail.com>
2020-03-08 12:09:53 -0600

    Tweeked master inventory

--- js/Scenes/InventoryScreen.js
+++ js/Scenes/InventoryScreen.js
@@ -19,15 +19,15 @@ var weaponInventoryMaster = [-1,//Basic Shot
 							 1,//Sniper Shot
 							 2,//Hop
 							 2,//Teleport Shot
-							 3,//Big Shot
-							 3,//Roller
+							 2,//Big Shot
+							 2,//Roller
 							 1,//Crazy Bomb
 							 2,//Meteor Clash
-							 5,// Rain Shot
+							 2,// Rain Shot
 							 3,// Ground Shot
 							 3,// Grenade
-							 3,// Delayed Multi Shot
-							 1]//Self Destruct 
+							 2,// Delayed Multi Shot
+							 0]//Self Destruct 
 
 function modeInventory(frameTime) {
 	colorRect(0, 0, canvas.width, canvas.height, "purple");
