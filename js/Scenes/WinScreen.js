1a5037ecb0010d1af9e17111eaf653e5fbfb379b
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-03-09 14:44:20 +0100

    Fixes: multiple call to startGame() when switching from winscreen to a new game (chaining campain mode levels)

--- js/Scenes/WinScreen.js
+++ js/Scenes/WinScreen.js
@@ -5,14 +5,13 @@ function modeWinScreen(frameTime) {
 	//colorRect(0, 0, canvas.width, canvas.height, "blue");
 	colorText("WIN SCREEN", canvas.width/2, 100, "White", "50px Arial",true);
     colorText("[Space Bar] MAIN MENU", canvas.width/2, canvas.height - 50, "white", "20px Arial", true);
-    
+
     stats.drawWinScreen();
 
-	if (Key.isJustPressed(Key.SPACE) 
+	if (Key.isJustPressed(Key.SPACE)
 		|| Key.isJustPressed(Key.q)
 		|| (isMouseInArea(0, canvas.height - 50, canvas.width, 50) && mouseJustPressed)){
         mode = MAIN_MENU;
 		map.init(canvas.width, canvas.height-UI_HEIGHT);
-		startMatch();
 	}
 }
