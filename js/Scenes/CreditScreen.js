ec0b05e9693a7004fa926026a9da590fffff8e44
Gamkedo <chris@gamkedo.com>
2020-03-16 10:17:30 -0700

    Updating credits (Simon's)

--- js/Scenes/CreditScreen.js
+++ js/Scenes/CreditScreen.js
@@ -6,7 +6,7 @@ var creditsScrollRate = 0.48;
 var creditNameList = [
 " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",
 "Michael \"Misha\" Fewkes: Project lead, core gameplay, initial terrain randomization, tank physics, majority of weapon types (jetpack, big shot, teleport shot, rolling shot, crazy bomb...), tank destruction desaturation, skin sprite sheet support, sound integration and related randomization, AI, button graphics, support for variable player count, song randomization, crosshair, terrain variation options, assorted bug fixes"," ",
-"Simon J Hoffiz: Majority of menu code, turns support, game clock, time of day, cloud drift, weapons (meteor clash, rain shot), star particles, damage indicator, grenade weapon physics fixes, assorted UI improvements and related refactoring"," ",
+"Simon J Hoffiz: Initial infrastructure work of all game screens, majority of code for main menu, turn/damage/other in-game indicators, game clock, day-night cycle, cloud movement, weapons (\"meteor clash\", \"grenade\", \"rain shot\"), night star and rain particles, some art implementation, campaign design and coding, weather system and UI modifications"," ",
 "I-wei Chen: Unified button functionality, start screen setup, logo animation, UI layout improvements, pause bug fix, slider widget, toggle button support, volume controls, scrolling menu background effect, graphic bug fixes"," ",
 "Jeff \"Axphin\" Hanlon: Title screen support, metal and screw tile decorations for edges, logo, sound effects (20), 6 tank skins, button styling, game over screen"," ",
 "Muhammed \"EyeForcz\" Durmusoglu: Mouse-only and Reflex-Click controls, Control Menu, winscreen background trophy model, credits screen background memorial model and support"," ",
